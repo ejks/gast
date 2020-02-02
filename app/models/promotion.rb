c4877da761c6474be3a66feb624fb040af308fdd
KittyChen <kimikamis@gmail.com>
2020-02-02 17:56:09 +0800

    set promotion / vendor model

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,6 @@
 class Promotion < ApplicationRecord
   belongs_to :vendor
   has_many :deals
+
+  enum status: { draft: 0, onsale: 1, trash: 2 }
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,4 +2,14 @@ class Vendor < ApplicationRecord
   belongs_to :user
   has_many :promotions
   has_many :deals, through: :promotions
+
+  enum vendor_type: { not_assigned: 0, restaurant: 1, bar: 2, cafe: 3, grocery_store: 4 }
+
+  def vendor_type_int
+    vendor_types[vendor_type]
+  end
+
+  def saved_promotions
+    promotions.where(status: ['draft', 'onsale'])
+  end
 end
